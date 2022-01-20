27b1f4aca3453b81c3813a3aba688f6f11ec340d
KruzhokProg <Aa1@3$5^7>
2022-01-20 11:45:36 +0300

    исправили ошибку диапазона 2

--- main.py
+++ main.py
@@ -16,55 +16,65 @@ def merge_time(data):
 
 data = open("26.txt").read().splitlines()[1:]
 # print(data)
-week_time = 7 * 24 * 60 * 60
+week_time = 7 * 24 * 60 * 60  # 604800
 start_week = 1634515200
+time_process = [0] * week_time  # 0...604799
+end_week = start_week + week_time
+ends = [0] * week_time  # 0...604799
+error = False
 # start_time = 1633046400
-res = [list(map(int, item.split(' '))) for item in data]
+# res = [list(map(int, item.split(' '))) for item in data]
 # test_range = [[2, 5], [5, 6], [1, 2], [3, 6]]
-# res = [[1634515199, 1635120001], [1634515198, 1635110000], [1634515210, 1635115000], [1634515100, 1634515150],
-#        [1635120010, 1635120050], [1635110000, 1635130000]]
+res = [[start_week - 10, start_week + 10], [start_week - 10, start_week - 9], [start_week - 10, end_week + 10],
+       [start_week + 10, start_week + 20], [start_week + 20, start_week + 30],
+       [end_week - 10, end_week + 10], [start_week + 10, end_week + 10]]
+
+# [-10, 10] -> [0, 10]
+# [-10, -9] ->
+# [-10, 604800] -> [0, 604799]
+# [10, 20] -> [10, 20]
+# [604790, 604800] -> [604790, 604800]
+# [10, 604800] -> [10, 604800]
+#
 # merge_time(test_range)
 # print(test_range)
-time_process = [0] * week_time
-end_week = start_week + week_time
-ends = [0] * week_time
-error = False
+
 for pos, item in enumerate(res):
     try:
         print(pos)
         a, b = item[0], item[1]
-        # b = week_time if b == 0 or b > end_week else b
         if b == 0:
-            b = week_time
+            b = week_time - 1
         elif b > start_week:
-            if b > end_week:
-                b = week_time
+            if b >= end_week:
+                b = week_time - 1
             else:
                 b -= start_week
         else:
             b = -1
 
-        # a = 0 if a == 0 or a < start_week else a - start_week
-        if a < start_week:
+        if a <= start_week:
             a = 0
         else:
             if a > end_week:
                 b = -1
+            elif a == end_week:
+                a = end_week - 1
             else:
                 a -= start_week
 
         if b - a >= 0:
             if ends[a] == 1:
                 ends[a] = 0
-                if b == week_time:
-                    b -= 1
+                # if b == week_time:
+                #     b -= 1
                 ends[b] = 1
                 a += 1
             elif ends[a] == 0:
-                if b == week_time:
-                    b -= 1
+                # if b == week_time:
+                #     b -= 1
                 ends[b] = 1
-            for i in range(a, b):
+            for i in range(a, b + 1):
                 time_process[i] += 1
     except Exception as ex:
         print(pos)
